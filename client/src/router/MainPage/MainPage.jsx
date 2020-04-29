bd11d11fceb8aa86933a7702a5a2b799d54cab21
max-osadchiy <max_cyber@ukr.net>
2020-04-29 22:35:11 +0300

    Make ThirdBlock in MainPage

--- client/src/router/MainPage/MainPage.jsx
+++ client/src/router/MainPage/MainPage.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import FirstBlock from '../../components/MainPage/FirstBlock/FirstBlock';
+import SecondBlock from '../../components/MainPage/SecondBlock/SecondBlock';
+import ThirdBlock from '../../components/MainPage/ThirdBlock/ThirdBlock';
 
 import './MainPage.scss';
-import SecondBlock from '../../components/MainPage/SecondBlock/SecondBlock';
 
 const MainPage = () => (
     <div className="main-page">
         <FirstBlock />
         <SecondBlock />
+        <ThirdBlock />
     </div>
 )
 
